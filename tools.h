3843562dbb9cf82dddf6fdca1e0158290696d467
humwerthuz <heineken69addict@gmail.com>
2014-04-22 13:06:07 -0300

    removed unnecesary macros

--- tools.h
+++ tools.h
@@ -2,10 +2,6 @@
 #define TOOLS_H
 #include <string.h>
 
-//Algunas macros para el manejo de strings
-#define _STR_CPY(to,from) to=(char*)malloc(sizeof(from));strcpy(to,from)
-#define _STRING_COPY _STR_CPY
-#define _CHAR_COPY(to,from) strcpy(to,from)
-#define _NEWCHAR(to,size) to=(char*)malloc(size + 1)
+#define _STRING_COPY(ptr,from) ptr=(char*)malloc(sizeof(from));strcpy(ptr,from)
 
 #endif
