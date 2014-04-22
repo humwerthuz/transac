b1829d96144ea9382fdb6f654777cc67ae659d18
humwerthuz <heineken69addict@gmail.com>
2014-04-22 13:06:37 -0300

    other test case

--- test.c
+++ test.c
@@ -5,8 +5,9 @@ int main(){
 
 	if(db_connect("humberto", "humberto", "asdasd")){
 		printf("%s\n", "Failed to connect to database");
+		return 1;
 	}
-	test *t = select_test_by_id(2);
+	test *t = select_test_by_id(1);
 	printf("Test name: %s, email: %s\n", t->name, t->email);
 	return 0;
 }
