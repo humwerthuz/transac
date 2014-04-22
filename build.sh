a3948019c1b6ffcac866f0d2dabdfa8cc630bc0d
humwerthuz <heineken69addict@gmail.com>
2014-04-22 13:05:29 -0300

    Added usage

--- build.sh
+++ build.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+
+function exit_with_error(){
+	echo "*** Failed! ";
+	exit 1;
+}
+
 NAME='db_wrapper'
-export LD_LIBRARY_PATH=/usr/pgsql-9.0/lib
+
+if [ ! -f $NAME.c ]; then
+	echo "File" $NAME.c "Does not exist in current directory."
+	exit_with_error
+fi
+
 echo "*** Preprocessing SQL "
-ecpg -o _$NAME.c $NAME.c
+ecpg -o _$NAME.c $NAME.c && echo "*** Sucess!" || exit_with_error
+
 echo "*** Building object code..."
-cc -I /usr/include/postgresql  -c _$NAME.c -o $NAME.o -L /usr/lib/postgresql/9.3/lib
-echo "*** '$NAME.o' Generated!"
+cc -I /usr/include/postgresql  -c _$NAME.c -o $NAME.o -L /usr/lib/postgresql/9.3/lib  && \
+echo "*** '$NAME.o' Generated!" || exit_with_error
+
